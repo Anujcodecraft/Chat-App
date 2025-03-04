5d51a25a2b36edb5f55b535fd8d9673bc05849ff
Anujcodecraft <ag083519@gmail.com>
2025-03-05 03:28:39 +0530

    added deployed link of backend and socket

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -26,7 +26,7 @@ export const ChatContextProvider = ({children, user}) => {
     
     // initializing socket
     useEffect(()=>{
-        const newSocket = io("http://localhost:3000");
+        const newSocket = io("https://chat-app-1-8sbc.onrender.com");
         setSocket(newSocket);
         return ()=>{
             newSocket.disconnect();
